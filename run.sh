4a905626a629009b6b0a8ed8a5ee934becd56c2d
cyfran <eremiya@gmail.com>
2022-03-29 02:46:20 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sed -i "s/:OUTPUT DROP/:OUTPUT ACCEPT/g" /etc/sysconfig/iptables
 systemctl restart iptables
+sleep 10
 myip=$(dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}')
 sed -i "s/123.234.345.456/$myip/g" /etc/rtpengine/rtpengine.conf
 sed -i "s/10.10.100.19/$myip/g" /etc/kamailio/kamailio.cfg
