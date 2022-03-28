635abc10e949d128088ed2e2608c7bdeae187c44
cyfran <eremiya@gmail.com>
2022-03-29 01:33:45 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,3 +3,4 @@
 myip=$(dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}')
 sed -i "s/123.234.345.456/$myip/g" /etc/rtpengine/rtpengine.conf
 sed -i "s/10.10.100.19/$myip/g" /etc/kamailio/kamailio.cfg
+echo > run.sh
