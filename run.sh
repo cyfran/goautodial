28bc081d1a09d63f318552033e8472acffdb415a
cyfran <eremiya@gmail.com>
2022-03-29 03:02:56 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-sed -i "s/:OUTPUT DROP/:OUTPUT ACCEPT/g" /etc/sysconfig/iptables
-systemctl restart iptables
-
-sleep 10
-
 yum -y install goautodial-ce
 cd /usr/src/goautodial
 ./install.sh
+sed -i "s/:OUTPUT DROP/:OUTPUT ACCEPT/g" /etc/sysconfig/iptables
+systemctl restart iptables
 
 myip=$(dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}')
 sed -i "s/123.234.345.456/$myip/g" /etc/rtpengine/rtpengine.conf
