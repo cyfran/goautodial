9f75a729579c92d6c0e3edb162b10a57dfbcdd56
cyfran <eremiya@gmail.com>
2022-03-30 21:41:31 +0300

    Create goCRMAPISettings.php

---
+++ goCRMAPISettings.php
@@ -0,0 +1,6 @@
+<?php
+define ('gourl', 'https://YOUR_SERVER_IP_ADDRESS/goAPIv2');
+define ('goUser', 'goAPI');
+define ('goPass', 'KToB93bzjGd1RS4mDqePJ6Uk.jgNRrK');
+define ('responsetype', 'json');
+?>
