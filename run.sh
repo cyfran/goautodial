a5e6da9fa73d23b78277dad4be5f159e06d12656
cyfran <eremiya@gmail.com>
2022-03-30 21:43:41 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-cd /var/www/html
-git config --global user.email "root@localhost" 
-git stash
-git pull
-
-cd /var/www/html/goAPIv2
-git stash
-git pull
-
-#sed -i "s/:OUTPUT DROP/:OUTPUT ACCEPT/g" /etc/sysconfig/iptables
-#systemctl restart iptables
-
-#myip=$(dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}')
-#sed -i "s/123.234.345.456/$myip/g" /etc/rtpengine/rtpengine.conf
-#sed -i "s/10.10.100.19/$myip/g" /etc/kamailio/kamailio.cfg
-
+#cd /var/www/html
+#git config --global user.email "root@localhost" 
+#git stash
+#git pull
+#cd /var/www/html/goAPIv2
+#git stash
+#git pull
+#wget -O "/var/www/html/php/Config.php" "https://raw.githubusercontent.com/cyfran/goautodial/main/Config.php"
+#wget -O "/mysqlfix.sql" "https://raw.githubusercontent.com/cyfran/goautodial/main/mysqlfix.sql"
+#mysql < /mysqlfix.sql
+#rm -f /mysqlfix.sql
 echo > /run.sh
