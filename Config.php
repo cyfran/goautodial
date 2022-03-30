dc68b84c8412798ca99f58a4b62cae77c64c7bb4
cyfran <eremiya@gmail.com>
2022-03-30 21:04:13 +0300

    Create Config.php

---
+++ Config.php
@@ -0,0 +1,19 @@
+<?php
+// database configuration
+define('DB_USERNAME', 'goautodialu');
+define('DB_PASSWORD', 'goautodialu1234');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'goautodial');
+define('DB_PORT', '3306');
+define('DB_NAME_ASTERISK', 'asterisk');
+
+define('DB_NAME_ASTERISK', 'asterisk');
+define('DB_USERNAME_KAMAILIO', 'kamailiou');
+define('DB_PASSWORD_KAMAILIO', 'kamailiou1234');
+define('DB_HOST_KAMAILIO', 'localhost');
+define('DB_NAME_KAMAILIO', 'kamailio');
+define('DB_PORT_KAMAILIO', '3306');
+
+// other configuration parameters
+define('CRM_ADMIN_EMAIL', 'admin@localhost.com');
+?>
