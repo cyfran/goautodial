7d158c70f7ab894a738b5343e31a43146f6853c5
cyfran <eremiya@gmail.com>
2022-03-30 15:47:01 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+cd /var/www/html
+git config --global user.email "root@localhost" 
+git stash
+git pull
+
+cd /var/www/html/goAPIv2
+git stash
+git pull
+
 #sed -i "s/:OUTPUT DROP/:OUTPUT ACCEPT/g" /etc/sysconfig/iptables
 #systemctl restart iptables
 
@@ -7,4 +16,4 @@
 #sed -i "s/123.234.345.456/$myip/g" /etc/rtpengine/rtpengine.conf
 #sed -i "s/10.10.100.19/$myip/g" /etc/kamailio/kamailio.cfg
 
-#echo > run.sh
+echo > /run.sh
